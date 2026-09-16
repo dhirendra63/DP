9ff4b53227ead0e143868156b321f6627080855b
Dhirendra <i.dhirendra63@gmail.com>
2026-09-16 14:04:01 +0530

    Implement house robber solution using dynamic programming

---
+++ houseRobber.java
@@ -0,0 +1,14 @@
+ public int solve(int[]nums,int i,int[]dp){
+    int n=nums.length;
+    if(i>=n) return 0;
+    if(dp[i]!=-1) return dp[i];
+    int house1=solve(nums,i+1,dp);
+    int house2=nums[i]+solve(nums,i+2,dp);
+    return dp[i]=Math.max(house1,house2);
+  }
+    public int rob(int[] nums) {
+        int n=nums.length;
+        int []dp=new int[n+1];
+       Arrays.fill(dp,-1);
+    return solve(nums,0,dp);
+    }
